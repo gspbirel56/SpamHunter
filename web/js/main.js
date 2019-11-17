caebb8e993ca56e3155b9e157fe3e80426ba581d
Chris <gspbirel56@gmail.com>
2019-11-17 00:00:33 -0500

    Added functions to get prediction from server given a message body

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,49 @@
 function getPrediction() {
-    document.getElementById("prediction").innerText = "ham"
+    var url = "http://localhost:8080"
+    var endpoint = "/predict"
+
+    var http = new XMLHttpRequest();
+
+    http.open("GET", url + endpoint, true);
+
+    http.onreadystatechange = function() {
+        var DONE = 4;       // 4 means that the request is done
+        var OK = 200;       // 200 means a successful return
+        if (http.readyState == DONE && http.status == OK && http.responseText) {
+            // JSON string
+            replyString = http.responseText;
+
+            // JSON -> JS obj
+            replyObj = JSON.parse(replyString);
+
+            document.getElementById("prediction").innerText = replyObj.prediction;  // signifies that we need a string called prediction from the backend
+        }
+
+    }
+}
+
+function getTopFiveAlgorithms() {
+    var url = "http://localhost:8080"
+    var endpoint = "/getAlgorithms"
+
+    var http = new XMLHttpRequest();
+
+    http.open("GET", url + endpoint, true);
+
+    http.onreadystatechange = function () {
+        var DONE = 4;       // 4 means that the request is done
+        var OK = 200;       // 200 means a successful return
+
+        if (http.readyState == DONE && http.status == OK && http.responseText) {
+            // JSON string
+            replyString = http.responseText;
+
+            // JSON -> JS object
+            replyObj = JSON.parse(replyString);
+
+            // put the algorithm names in their ranked order
+
+            // put the algorithms' F1 scores in their ranked order
+        }
+    }
 }
