e185f4a90ca89fdd1152e2fce62c58705a185fe8
Chris <gspbirel56@gmail.com>
2019-11-17 20:13:53 -0500

    Two JS fns on load

--- web/js/main.js
+++ web/js/main.js
@@ -63,8 +63,51 @@ function getTopFiveAlgorithms() {
     }
 
     // for debug purposes until the server communication is implemented
-    var placeholder_name = "qaoweifj";
-    var placeholder_f1 = "75.43%";
-    document.getElementById("alg1_name").innerText = placeholder_name;
-    document.getElementById("alg1_f1").innerText = placeholder_f1;
+    //var placeholder_name = "qaoweifj";
+    //var placeholder_f1 = "75.43%";
+    //document.getElementById("alg1_name").innerText = placeholder_name;
+    //document.getElementById("alg1_f1").innerText = placeholder_f1;
+}
+
+function getTopFiveWords() {
+    var url = "http://localhost:8080"
+    var endpoint = "/getWords"
+
+    var http = new XMLHttpRequest();
+
+    http.open("GET", url + endpoint, true);
+
+    http.onreadystatechange = function () {
+        var DONE = 4;       // 4 means that the request is done
+        var OK = 200;       // 200 means a successful return
+
+        if (http.readyState == DONE && http.status == OK && http.responseText) {
+            // JSON string
+            replyString = http.responseText;
+
+            // JSON -> JS object
+            replyObj = JSON.parse(replyString);
+
+            // put the algorithm names in their ranked order
+            document.getElementById("alg1_name").innerText = replyObj.name[0];
+            document.getElementById("alg2_name").innerText = replyObj.name[1];
+            document.getElementById("alg3_name").innerText = replyObj.name[2];
+            document.getElementById("alg4_name").innerText = replyObj.name[3];
+            document.getElementById("alg5_name").innerText = replyObj.name[4];
+
+            // put the algorithms' F1 scores in their ranked order
+            document.getElementById("alg1_f1").innerText = replyObj.f1[0];
+            document.getElementById("alg2_f1").innerText = replyObj.f1[1];
+            document.getElementById("alg3_f1").innerText = replyObj.f1[2];
+            document.getElementById("alg4_f1").innerText = replyObj.f1[3];
+            document.getElementById("alg5_f1").innerText = replyObj.f1[4];
+        }
+        else {
+            console.log("Error getting ranked algorithms/F1 scores from the backend server. (Is it running?)");
+        }
+}
+
+function initializeIndex() {
+    getTopFiveAlgorithms();
+    getTopFiveWords();
 }
